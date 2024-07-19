db692e028798a667d9b39bb0663021877f3b2531
imuhammmadh <imuhammmadh@gmail.com>
2024-07-19 07:40:16 +0530

    Introduction

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-let arr = [1, 2, 3, 4, 5]
+//Introduction
+function iterator(a) {
+    let nextIndex = 0
+    return {
+        next: function () {
+            if (nextIndex < arr.length) {
+                return {
+                    value: arr[nextIndex++],
+                    done: false
+                }
+            }
+            else {
+                return { done: true }
+            }
+        }
+    }
+}
+let arr = [1, 2, 3, 4]
+let i = iterator(arr)
+console.log(i.next().value);
+console.log(i.next().value);
+console.log(i.next().value);
+console.log(i.next().value);
